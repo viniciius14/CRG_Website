b3286e06ed5023bda6e9abdfb1a2ab5d3d7f177e
viniciius14 <rodri.f.ribeiro@gmail.com>
2022-03-24 14:57:25 +0000

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+        TODO LIST
+
+-border radius != 0 ?
+-make gif proportional to the users view
+-align text to the bottom left or right
+-make gifs activate only when the user is seeing them (atleast 75% in view)
+    -if the user isnt seeing them replace with a static image
+-put all the movie titles and text
+-photo and info about the director with maybe a vid about him from youtube
+-photo and info about the studio
+-see if it's fisable to add sound to the gifs
+
+*/
