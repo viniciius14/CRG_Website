4c88d22b427bb2b65e9cdf2818820e9e4ae429e4
viniciius14 <rodri.f.ribeiro@gmail.com>
2022-03-30 09:56:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 /*
         TODO LIST
 
--make gif proportional to the users view
--align text to the bottom left or right
+
 -make gifs activate only when the user is seeing them (atleast 75% in view)
     -if the user isnt seeing them replace with a static image
 -put all the movie titles and text
 -photo and info about the director with maybe a vid about him from youtube
 -photo and info about the studio
 -see if it's fisable to add sound to the gifs
+
+
+
+    MOVIES TO ADD
+
+-howls moving castle
+-mononoke
+-kikis
+-arriety
+-spirited away
 
 */
