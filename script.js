b16a00fe521fe7b0e5cae365154d2d0fdd51cf7f
viniciius14 <rodri.f.ribeiro@gmail.com>
2022-04-05 22:16:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,43 @@
         TODO LIST
 
 
--make gifs activate only when the user is seeing them (atleast 75% in view)
-    -if the user isnt seeing them replace with a static image
+
+
+
+
+-texto da kikis delivery ta meio estrano, arranjar maneira de o meter mais natural
+-mudar cores
+-descricao totoro
+-mapa (em baixo como o adicionar)
+    1. Google Maps
+    Google Maps is the de facto map service on the planet. Daily, over a billion people tap into Google Maps to navigate their way around town. If not for China’s Great Firewall, Google Maps would be the most popular mapping tool in every country. Here’s a secret: embedding Google maps onto your web app takes less than three minutes. Here’s what you do:
+
+    Open Google Maps.
+    Go to the map (or Street View) of your location.
+    Click Menu (top left).
+    Click Share or embed map.
+    Click Embed map.
+    Pick the size you want by clicking the down arrow to the left of the text field.
+    Copy the displayed HTML code. Paste it into your web app.
+
+-directors ta mt a esquerda e devia tar centrado
+-escolher fonte de escrita
+    -fonts.google.com
+    -vid usa "*{font-family: 'Poppins', sans-serif;}" no ficheiro de css *{} faz com q as mudancas ai sejam aplicadas a tudo
+
+
+
+
+
+
+
+
+
+
+
 -put all the movie titles and text
 -photo and info about the director with maybe a vid about him from youtube
 -photo and info about the studio
--see if it's fisable to add sound to the gifs
 
-
-
-    MOVIES TO ADD
-
--howls moving castle
--mononoke
--kikis
--arriety
--spirited away
 
 */
